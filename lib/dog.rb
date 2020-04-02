089feed6cddc72e12590e5cb60bbba8e498d941f
Zaire  Swailes <swaileszaire@gmail.com>
2020-04-02 08:00:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Add your code here
+class Dog 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    self >> @@all 
+  end
+    
+  @@all = [] 
+    
+    
+  def self.all 
+    return self.all 
+  end
+  
+  def self.print_all 
+  end 
+
+  def self.save 
+    self >> @@all 5
+  end
+end
+  
